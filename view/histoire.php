497e0b57a380239b2067ff8cb8d561e5a34db173
Tiro-Tupea <t.tupea@outlook.be>
2026-04-07 15:01:46 +0200

    page histoire fini

--- view/histoire.php
+++ view/histoire.php
@@ -28,6 +28,122 @@
                 <h2>Histoire</h2>
             </div>
 
+            <img src="../img/histoire.jpg" alt="histoire">
+
+            <p>Le nom de la ville est dû selon la tradition à la légende des jumeaux Romulus et Rémus, où le premier tua le second, obtenant ainsi le droit de donner son nom à la ville qu'il construisait. Une autre hypothèse suggère que le nom de Rome viendrait du mot Rumon, nom étrusque du Tibre, qui serait ainsi devenue « La ville du fleuve »10. La date historique de la fondation de la ville est le 21 avril de l'an 753 av. J.-C. (date qui a été retenue comme celle du début du calendrier romain).</p>
+
+            <p>Surnoms de Rome : La Ville aux Sept Collines, La Ville Éternelle, Caput mundi (la capitale du monde).
+            Les symboles de Rome sont :
+            la louve capitoline, inspirée par la légende de la fondation de Rome ;
+            SPQR (le Sénat et le peuple romain) : la devise antique, qui est toujours celle de la ville de Rome : elle fait partie du blason de la ville, et figure toujours sur les édifices publics, bâtiments et bouches d'égout.
+            Les couleurs de Rome sont le rouge et le jaune (la pourpre et l'or).</p>
+
+            <h4>Origine légendaire</h4>
+
+            <p>La fondation de Rome est légendaire ; elle est attribuée à Romulus, descendant d'Enée, qui aurait été élevé par une louve, avec son frère Rémus. La tradition populaire généralement admise – et reprise aussi par Varron (qui dans le De lingua latina jeta les bases de l'étude linguistique du peuple latin) – veut qu'elle ait été fondée par Romulus le 21 avril 753 av. J.-C. ; après avoir tué son frère.</p>
+
+            <p>D'un point de vue scientifique, on admet que Rome est née de l'association des tribus de la région (et de la réunion des villages qui occupaient le sommet des collines) vers le viiie siècle av. J.-C. ; il faut y voir une décision stratégique et politique plutôt que la construction d'une cité ex nihilo. Ainsi commença la période de la « Rome carrée », ainsi nommée d'après la forme carrée du mont Palatin, même si on ne peut pas encore parler d'une véritable ville. Au cours du viiie siècle av. J.-C., l'urbanisation gagne les grands centres italiens, formant un chapelet de cités commerciales, unies par les échanges mais non fédérées. L'enjeu stratégique de la future Rome explique en grande partie la domination progressive de la culture étrusque dans cet embryon de ville. Rapidement, la société se diversifie, la population croît, et la monarchie s'impose.</p>
+
+            <p>Romulus sera le premier roi de Rome et six autres monarques lui succéderont, eux aussi probablement d’origine étrusque : Numa Pompilius, Tullus Hostilius, Ancus Marcius, Ostie, Tarquin l'Ancien, Servius Tullius. Le dernier roi fut Tarquin le Superbe qui, à cause de son comportement arrogant et de son mépris envers les citoyens et les institutions romaines, sera chassé par le peuple en 509 av. J.-C</p>
+
+            <h4>République romaine</h4>
+
+            <p>Après l'an 509 av. J.-C., Rome s'allia avec d'autres villes latines pour leur défense commune contre les incursions des Sabins, tandis qu’entre-temps, avant 400 av. J.-C., la puissance étrusque était confinée à l'intérieur de l'Étrurie. Rome commençait à émerger comme la ville dominante du Latium, mais en 387 av. J.-C. (ou en 390 ?) elle fut mise à sac par des envahisseurs gaulois conduits par Brennos qui avait déjà envahi l'Étrurie. Par la suite et durant toute l'époque républicaine, l’État romain reprit l'offensive et mena une longue série de guerres : il conquit l'Étrurie, prit des territoires aux Gaulois dans le nord et soumit les autres Latins et les populations samnites au sud. En 290 av. J.-C. plus de la moitié de la péninsule était dominée par Rome. Au milieu du iiie siècle av. J.-C. même les cités grecques furent soumises.</p>
+
+            <p>Les problèmes induits par une expansion aussi importante que soudaine que dut affronter la République furent énormes et de plusieurs sortes : les institutions romaines étaient jusqu'alors conçues pour administrer une cité-État ; désormais les provinces (comparables aux colonies des États modernes, à ne pas confondre avec les colonies romaines proprement dites, lesquelles étaient des implantations de citoyens romains à plein titre, cives optimo iure, hors du territoire public de la République mais relevant de l'administration directe de l'État romain) s'étendaient de l'Ibérie, à l'Afrique, à la Grèce et à l'Asie.</p>
+            
+
+            <p>Le contact avec une civilisation grecque plus évoluée et l'arrivée dans la ville de très nombreux esclaves helléniques (souvent plus cultivés et instruits que leurs maîtres) causèrent chez le peuple romain, en particulier dans la classe dirigeante, des sentiments et des passions ambivalents : d'une part on voulait (et créussi en grande partie à la fin) rajeunir, rénover, les mœurs rurales romaines -mos maiorum- en introduisant des usages et des connaissances en provenance de l'Orient, ce qui permit d'accroître de façon significative le niveau culturel des Romains, au moins chez les patriciens - introduction de la philosophie, de la rhétorique, de la littérature et de la science grecques - mais cela engendra aussi une « décadence » des valeurs morales traditionnelles, dont témoigne la diffusion de coutumes et d'habitudes nouvelles. Tout cela provoqua une vive résistance de la part des milieux les plus conservateurs de la communauté romaine. Ceux-ci se liguèrent contre les cultures étrangères accusées d'être responsable de la corruption des coutumes, d'indécence, d'immoralité, de sacrilège envers les rites religieux romains.</p>
+            
+
+            <p>Au ier siècle av. J.-C., la République commença à se fissurer, les personnages les plus influents, affirmant fortement leur pouvoir personnel et se faisant les interprètes des besoins des masses défavorisées ou de la nécessité de maintenir le pouvoir aux mains des gentes les plus importantes et les plus riches, conduisirent à la guerre civile. La République dut également affronter une révolte des esclaves menée par Spartacus. Celle-ci est écrasée au prix de plusieurs massacres : Pompée exterminera cinq mille esclaves en Etrurie et Crassus en fera crucifier six mille autres, tout au long de la Via Appia. D'autres conquêtes suivirent, la Gaule par Jules César, et les Romains allèrent jusqu'en Syrie et en Arménie.</p>
+            
+
+            <h4> Histoire médiévale</h4>
+           
+
+            <p>La chute de l'Empire romain d'Occident ne changea pas beaucoup les choses pour Rome. Odoacre et donc les Ostrogoths continuèrent, comme les autres empereurs, à gouverner l'Italie depuis Ravenne. Entre-temps, le Sénat, bien que privé depuis longtemps de ses pouvoirs, continuait seul à administrer Rome, et le Pape était généralement issu d'une famille sénatoriale. Cette situation perdura jusqu'à ce que l'Empire romain d'Orient, sous le règne de Justinien Ier, prit la ville en 536.</p>
+            
+
+            <p>Cependant la confrontation entre les Ostrogoths et les Byzantins s'exacerba, entraînant une série de guerres qui dévastèrent Rome et les territoires environnants.</p>
+            
+            <p>De toute façon, le pape était devenu l'une des principales figures religieuses de tout l'empire byzantin, et en réalité plus puissant à Rome que les sénateurs restants, ou que les officiers byzantins. En pratique, le pouvoir local à Rome était dans les mains du pape, et au cours des décennies suivantes, beaucoup des possessions de l’aristocratie sénatoriale et de l’administration byzantine furent absorbées par l’Église.</p>
+            
+
+            <p>En 663, Rome reçut la visite de Constant II, première visite impériale depuis deux siècles. Mais le profond fossé culturel (l'empereur parlait grec) ainsi que le peu d'aide concrète apportée firent de ce voyage un fiasco. Pendant le demi-siècle qui suivit, à part quelques tensions, Rome et la papauté préférèrent continuer à suivre le gouvernement byzantin, en partie parce que l'alternative était le gouvernement lombard, et en partie parce que l'approvisionnement des Romains venait pour l'essentiel de domaines de la papauté situés dans d'autres régions de l’Empire, en particulier en Sicile.</p>
+            
+            <p>En 727, le pape Grégoire II refusa d'accepter le décret de l’empereur Léon III qui établissait l'iconoclasme. Léon tenta, sans succès, d'imposer l'iconoclasme à Rome par la force des armes. Il confisqua les domaines du pape en Sicile et transféra les terres précédemment ecclésiastiques de l’empire au patriarche de Constantinople. En pratique, Rome était expulsée de l'Empire byzantin.</p>
+            
+
+            <p>Cela laissa Rome complètement aux mains des forces locales pour sa protection contre les invasions des Lombards, désormais encouragées même par les Byzantins. De nouvelles protections étaient nécessaires, et finalement, en 753, le pape Étienne II poussa Pépin le Bref, roi des Francs à attaquer les Lombards avec la bénédiction de la papauté.</p>
+            
+
+            <p>Le lundi 23 août 846, une flotte barbaresque accoste à l'embouchure du Tibre, met à sac la ville d'Ostie, puis lance un raid terrestre qui pille la basilique Saint-Pierre de Rome et la basilique Saint-Paul-hors-les-Murs (voir Sac de Rome (846)), situées alors hors des murs de la Ville. Le pape Léon IV commanda alors la construction d'un mur défensif partant du mausolée d'Hadrien, contournant la basilique Saint-Pierre, puis revenant au Tibre. Financé par l'empereur Lothaire Ier qui ordonna un impôt spécial dans tout son empire, l'ensemble de cette fortification reçut le nom de cité léonine.</p>
+            
+            <h4>La Rome des papes et de la Renaissance</h4>
+            
+
+            <p>Les historiens parlent d’une sorte d'échange entre les milieux pontificaux et les Francs, les premiers concédant aux seconds, à Pépin le Bref d’abord et à Charlemagne ensuite, le charisme impérial, qui devait les rattacher à l'Empire romain « d'Occident » pour la défense de la chrétienté, et recevant en retour la reconnaissance d'une prétendue « donation de Constantin » en faveur du pouvoir temporel de l’Église sur la ville de Rome et les territoires avoisinants.</p>
+            
+
+            <p>Le pouvoir croissant du pape l'amena inévitablement à affronter l'Empire byzantin, irrité du rôle politique toujours plus ouvertement et librement assumé par l’Église, le conflit culmina dans la controverse sur l’iconoclastie, refusée par le pape Grégoire II. L'empereur byzantin Léon III, finit par exclure Rome de l'Empire, si bien que Rome ne put alors compter que sur son alliance avec les Francs, grâce auxquels la ville ne tomba pas aux mains des nombreux ennemis qui l'entouraient.</p>
+            
+
+            <p>C’est à cette époque que furent effectivement constitués les États pontificaux, dont Rome était la capitale, qui devint rapidement le centre mondial du christianisme de rite latin. Le pouvoir du pape ne lui permettait pas encore de gouverner ni de défendre l’État, mais il devait rapidement s'accroître et les expériences républicaines perdirent de l'importance, jusqu'à leur disparition complète. En outre, à l'époque de la Renaissance, Rome devint également un très important foyer culturel, qui disputait à Florence et à Venise les innovations artistiques les plus significatives.</p>
+            
+
+            <h4>Histoire contemporaine</h4>
+            
+
+            <p>À la fin du xviiie et au xixe siècle, les mouvements révolutionnaires caractérisant cette époque n'ont pas exclu Rome. Le gouvernement des papes fut interrompu par la courte existence de la République romaine (1798) instituée par des envoyés du Directoire. L'organisation politique était inspirée du modèle de la Révolution française. La ville de Rome fut également le chef-lieu du département de Rome durant l'Empire français, de 1809 à 1814.</p>
+            
+
+            <p>Une autre République romaine surgit en 1849, dans le courant des révolutions de 1848. Deux des plus influentes figures de l’unification italienne, Giuseppe Mazzini et Giuseppe Garibaldi, combattirent pour l'éphémère république Mazzini fut nommé triumvir avec Carlo Armellini et Aurelio Saffi.</p>
+            
+
+            <p>Le pape s’opposa au processus d’unification de l’Italie qui allait conduire à la réunification de toute la péninsule sous le contrôle des Savoie. Le retour du pape Pie IX à Rome, avec l'aide des troupes françaises en 1850, exclut Rome du processus d’unification qui se traduisit par la deuxième guerre d'indépendance italienne et par l’expédition des Mille, à la suite de laquelle toute la péninsule italienne, excepté Rome et Venise, fut réunifiée sous le règne des Savoie.</p>
+            
+            <p>En trente ans, jusqu’en 1900, la population a doublé, ainsi que la ville bâtie et Rome entre dans la civilisation moderne et reprend sa croissance.</p>
+            
+
+            <h4>Situation actuelle</h4>
+            
+
+            <p>Aujourd'hui, la population de la ville s'est stabilisée à 2,8 millions d'habitants, et son aire urbaine en compte à peu près 4 millions sur 5 352 km2. C'est la troisième agglomération d'Italie après Milan et Naples, mais la commune en elle-même est la plus peuplée du pays, et la quatrième de l'Union Européenne (après Londres, Berlin et Madrid).</p>
+            
+
+            <p>Des différences sociologiques existent selon les quartiers. Le nord (Tor di Quinto), le centre et le sud de la ville (EUR) sont aisés, tandis que la majeure partie des quartiers est (dont San Basilio et Alessandrino) sont populaires, tout comme Primavalle à l'ouest.</p>
+            
+
+            <p>Durant sa longue histoire, et étant donnée son importance, Rome a toujours eu une population caractérisée par d'importants flux migratoires ; ainsi, par tradition, un « vrai » Romain est une personne dont la famille a vécu à Rome depuis au moins sept générations. Aujourd'hui on considère qu'un individu né à Rome de parents nés à Rome est un « Romain de Rome », selon l'expression consacrée.</p>
+            
+
+            <p>La Rome d’aujourd’hui reflète les stratifications des époques de sa longue histoire, mais c'est aussi une grande métropole moderne. Le vaste centre historique contient de nombreux vestiges de la Rome antique, quelques vestiges de l'époque médiévale, de nombreux trésors artistiques de la Renaissance, nombre d’églises et de palais baroques, avec beaucoup d'exemples de l’Art nouveau, du néoclassicisme, du modernisme, du rationalisme et d’autres styles artistiques des XIXe et xxe siècle, la ville peut être considérée comme une sorte d’encyclopédie vivante de 3 000 ans d’art occidental.</p>
+            
+
+            <p>Le centre historique s’identifie avec les limites des antiques ousmoutou impériales. Certaines zones ont été restructurées à la suite de la réunification (1880-1910 Rome Umbertina), et quelques adjonctions et adaptations furent réalisées pendant la période fasciste, avec la création de la Via dei Fori Imperiali et Via della Conciliazione face au Vatican (pour la construction de laquelle une grande partie du Borgo adjacent fut détruite ; et la fondation de nouveaux quartiers (parmi lesquels Eur (construit en vue de l’Exposition universelle de 1942), San Basilio, Garbatella, Cineville, Trullo, Quarticciolo, et, sur la côte, la restructuration d'Ostie) et l'annexion des villages limitrophes (Labaro, Osteria del Curato, Quarto Miglio, Capannelle, Pisana, Torrevecchia, Ottavia, Casalotti). Cela a entraîné une extension vers le sud-est, le long des voies Tiburtine, Prenestine, Casiline, Appia Nouvelle. La ville a dépassé le cours de l’Aniene d'un côté et s'est étendue vers la mer de l'autre, au nord-ouest elle a englobé Monte Mario. Ces extensions étaient nécessaires pour faire face à la grande croissance démographique due à la centralisation de l’État italien.</p>
+            
+
+            <p>Durant la Seconde Guerre mondiale, Rome a souffert de lourds bombardements (notamment à San Lorenzo) et de batailles (Porta San Paolo, La Storta) et fut déclarée « ville ouverte ». Cependant, Rome évita une destruction totale comme à Berlin ou Varsovie. La ville tomba aux mains des Alliés le 4 juin 1944. Ce fut la première capitale d’une puissance de l’Axe à tomber.</p>
+            
+
+            <p>Après la guerre, Rome continua à s'étendre du fait de l'administration croissante et de l'industrie italienne, avec la création de nouveaux quartiers et faubourgs. La population actuelle est officiellement d'environ 2,8 millions d'habitants, mais les jours ouvrables, on estime qu'elle dépasse 3,5 millions. C'est une croissance notable comparée au passé, en effet le nombre d'habitants était de 138 000 en 1825, de 244 000 en 1871, de 692 000 en 1921 et de 1 600 000 en 1961. Tout autour de la ville, s’est créé un réseau de quartiers périphériques en continuelle expansion, ce qui a engendré une série de problèmes sociaux et économique.</p>
+            
+
+            <p>Rome accueillit les Jeux olympiques d'été de 1960, utilisant de nombreux sites antiques comme la villa Borghèse et les thermes de Caracalla pour les accueillir. Pour ces Jeux olympiques de nouvelles structures ont été créées, comme le Stadio Olimpico, le grand stade olympique (qui fut par la suite encore rénové et agrandi pour accueillir les qualifications et la finale de la Coupe du Monde de football de 1990 de la FIFA), le village olympique (créé pour héberger les athlètes et transformé après les jeux en un quartier résidentiel).</p>
+            
+
+            <h4>Beaucoup de monuments de Rome ont été restructurés par l’État italien et par le Vatican à l'occasion du Jubilé de 2000.</h4>
+
+            <p> Capitale de l’Italie, Rome accueille les principales institutions de la nation, comme la présidence de la République, le gouvernement et les ministères, le parlement, les principales cours judiciaires, et les délégations diplomatiques de toutes les nations auprès de l’État italien et du Vatican (curieusement, Rome accueille dans la partie italienne de son territoire, l’ambassade italienne auprès du Vatican, cas unique d’une ambassade installée à l’intérieur des frontières de son propre pays). De nombreuses institutions internationales ont leur siège à Rome, institutions culturelles, scientifiques ou humanitaires comme la FAO.</p>
+           
+
+            <p>Aux élections municipales du 19 juin 2016, pour la première fois de leur Histoire pluri-millénaires, les Romains élisent une femme à la tête de leur ville : Virginia Raggi (du Mouvement 5 étoiles).</p>
+            
+
+            <p>Aujourd’hui, Rome est l’une des plus importantes destinations touristiques du monde, grâce à son immense patrimoine archéologique et à ses trésors artistiques, mais aussi pour ses traditions uniques et la beauté de ses vues et de ses villas. Parmi les curiosités les plus intéressantes, il y a de nombreux musées (musées Capitolins, musée du Vatican, Galerie Borghèse, et beaucoup d’autres), des églises, des constructions historiques, les monuments et les ruines du Forum romain et des Catacombes.</p>
+            
+
         </section>
     </main>
 </body>
