fcaba138083b151f18d6691ad0ff8aa3a078b38a
Tiro-Tupea <t.tupea@outlook.be>
2026-04-07 10:57:56 +0200

    creation page acceuil

---
+++ public/index.php
@@ -0,0 +1,31 @@
+ <?php
+# public/index.php 
+/****************************
+ * Chargement des dépendances
+ * ici seulement config.php
+ * qui se trouve 1 niveau en
+ * dessous
+ ****************************/
+require_once '../config.php';
+// test de la constante racine
+// echo "Racine du projet : ".ROOT_PATH."";
+// affichage des pages acceptées
+// echo "<pre>";
+// print_r(ARRAY_VALID_PAGES);
+// echo "</pre>";
+
+// appel de la vue ( inclu la page acceuil a la page inde)
+if(!isset($_GET['p'])){
+
+    // Nous sommes sur l'accueil
+    // chargement de view/acceuil.php
+    include ROOT_PATH."/view/acceuil.php";
+
+    // sinon si
+}else if (in_array($_GET['p'], ARRAY_VALID_PAGES)){
+
+        include ROOT_PATH."/view/".$_GET['p'].".php";
+
+}else{
+    include ROOT_PATH."/view/error404.php";
+}
---
+++ view/inc/menu.php
@@ -0,0 +1,14 @@
+<?php
+
+# view/inc/menu.php
+?>
+<nav>
+    <ul class="nav-links">
+        <li><a href="./" class="active">Accueil</a></li>
+        <li><a href="./?p=geographie">Géographie</a></li>
+        <li><a href="./?p=histoire">Histoire</a></li>
+        <li><a href="./?p=culture">Culture</a></li>
+        <li><a href="./?p=contact">Contact</a></li>
+        <li><a href="./?p=liens">Liens</a></li>
+    </ul>
+</nav>
