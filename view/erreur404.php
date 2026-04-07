cbdddbafd4df41f161c1c571ac3b70ebb8637acd
Tiro-Tupea <t.tupea@outlook.be>
2026-04-07 14:14:22 +0200

    structure des autres pages fini

--- view/erreur404.php
+++ view/erreur404.php
@@ -1,15 +1,15 @@
 <?php
-// view/erreur404.php
+// view/contact.php
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="../css/style.css">
+    <title>Erreur</title>
 </head>
 <body>
-    <h1>Erreur 404</h1>
+   <img src="../img/404.jpg" alt="">
 </body>
 </html>
---
+++ view/histoire.php
@@ -0,0 +1,34 @@
+<?php
+// view/contact.php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/style.css">
+    <title>Histoire</title>
+</head>
+<body>
+    <header>
+        <div class="title">
+            <h1>ROME</h1>
+        </div>
+       
+    </header>
+    
+    <main>
+        <section class="menu">
+            <?php
+                include ROOT_PATH. "/view/inc/menu.php"
+            ?>
+        </section>
+        <section class="container">
+            <div class="container-title">
+                <h2>Histoire</h2>
+            </div>
+
+        </section>
+    </main>
+</body>
+</html>
