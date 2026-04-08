005cff479aa167e5f91ff29842ddb340bc40c361
Tiro-Tupea <t.tupea@outlook.be>
2026-04-08 10:22:34 +0200

    page galerie fini

--- view/galerie.php
+++ view/galerie.php
@@ -1,5 +1,5 @@
 <?php
-// view/contact.php
+// view/galerie.php
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -26,6 +26,62 @@
         <section class="container">
             <div class="container-title">
                 <h2>Galerie</h2>
+            </div>
+
+            <div class="gal">
+
+                <article class="left">
+
+                    <div class="img-box">
+                        <img src="../img/480px_equestrian-statue-of-marcus-aurelius.jpg" alt="">
+                        <div class="cap">
+                            <caption>Equestrian statue of marcus aurelius</caption>
+                        </div>
+                        
+                    </div>
+
+                    <div class="img-box"> 
+                        <img src="../img/480px_Fontana_de_Trevi" alt="">
+                        <div class="cap">
+                            <caption>Fontana de Trevi</caption>  
+                        </div>
+                        
+                    </div> 
+                    
+                    <div class="img-box">
+                        <img src="../img/480px_Panthéon,_Rome.jpg" alt="">
+                        <div class="cap">
+                            <caption>Panthéon</caption>
+                        </div>
+                         
+                    </div>
+                </article>
+
+                <article class="right">
+                    <div class="img-box">
+                        <img src="../img/480px_Piazza_Navona.jpg" alt="">
+                        <div class="cap">
+                            <caption>Piazza Navona</caption>
+                        </div>
+                        
+                    </div>  
+                    
+                    <div class="img-box">
+                        <img src="../img/480px_Pont_Saint-Ange_et_le_Vatican.jpg" alt="">
+                        <div class="cap">
+                            <caption>Pont Saint-Ange et le Vatican</caption>
+                        </div>
+                        
+                    </div>
+
+                    <div class="img-box">
+                        <img src="../img/480px_Quirinale_palazzo_e_obelisco.jpg" alt="">
+                        <div class="cap">
+                            <caption>Quirinale palazzo e obelisco</caption>
+                        </div>
+                        
+                    </div>
+                </article>
             </div>
 
         </section>
