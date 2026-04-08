68490a2d978c74d54d417aee35662fd0664befdc
Tiro-Tupea <t.tupea@outlook.be>
2026-04-08 11:11:34 +0200

    page contact fini

--- view/contact.php
+++ view/contact.php
@@ -27,6 +27,43 @@
             <div class="container-title">
                 <h2>Contact</h2>
             </div>
+            
+            <div class="forms">
+                <img src="../img/contacts.jpg" alt="">
+
+                <p><b>Pour nous laissez un commentaire ou obtenir un renseignement complémentaire, remplissez le formulaire ci-dessous : </b></p>
+
+                <form action="GET">
+                    <div class="form-box">
+                        <label for="lname" id="lname">NOM : </label>
+                        <input type="text" name="lname" id="lname">
+                    </div>
+                    
+                    <div class="form-box">
+                        <label for="fname" id="fname">PRENOM : </label>
+                        <input type="text" name="fname" id="fname">
+                    </div>
+                    
+                    <div class="form-box">
+                        <label for="city" id="city">VILLE : </label>
+                        <input type="text" name="city" id="city">
+                    </div>    
+                    
+                    <div class="form-box">
+                        <label for="mail" id="mail">E-MAIL : </label>
+                        <input type="email" name="email" id="email">
+                    </div>
+                    
+                    <div class="form-box comment">
+                        <label for="comm" id="comm">COMMENTAIRE : </label>
+                        <textarea name="comm" id="comm"></textarea>
+                    </div>
+                    
+                    <button type="submit">Envoyer votre message</button>
+
+                
+                </form>
+            </div>
 
         </section>
     </main>
